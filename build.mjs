7bde97f6dd0a68de6190b8ab6a2e56f495b3f3e9
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2025-03-03 16:48:22 -0600

    initial

---
+++ build.mjs
@@ -0,0 +1,61 @@
+import esbuild from 'esbuild'
+import svelte from 'esbuild-svelte'
+import { typescript } from 'svelte-preprocess-esbuild'
+
+const dev = true
+
+const main = async() => {
+	const context = await esbuild.context({
+		entryPoints: [ `./src/cool.mjs` ],
+		bundle: true,
+		outdir: `./dist`,
+		globalName: `cool`,
+		platform: `browser`,
+		conditions: [ `svelte`, `browser`, `module` ],
+		format: `iife`,
+		resolveExtensions: [ `.ts`, `.js`, `.json`, `.mjs`, `.cjs` ],
+		sourcemap: true,
+		minify: !dev,
+		external: [
+			`*.gif`,
+			`*.png`,
+		],
+		target: `es2022`, // browserslist_to_esbuild(`> .25% in US or last 2 years`),
+		plugins: [
+			svelte({
+				// cache: false,
+				compilerOptions: {
+					// dev: true,
+					// hydratable: false,
+					css: `external`,
+				},
+				// filterWarnings: w => !exclude_svelte_warning_codes.has(w.code),
+				preprocess: [
+					typescript(),
+				],
+			}),
+		],
+	})
+
+	const build = () => {
+		console.time(`built in`)
+
+		return context.rebuild()
+			.then(() => console.timeEnd(`built in`))
+			.catch(e => console.error(e))
+	}
+
+	await build()
+
+	await new Promise(resolve => setTimeout(resolve, 1000))
+
+	await build()
+
+	return context.dispose()
+}
+
+main().catch(e => {
+	console.error(`ERROR in cf_workers/www/barnacle/esbuild.config.mjs`)
+	console.error(e.message)
+	process.exit(1)
+})
---
+++ src/Cool.svelte
@@ -0,0 +1,13 @@
+<script lang='ts' generics='S extends string'>
+	export let whatever
+	// svelte-ignore export_let_unused
+	export let my_string: S
+</script>
+
+{whatever}
+
+<!-- svelte-ignore a11y_no_static_element_interactions a11y_click_events_have_key_events -->
+<div on:click={() => console.log(`clicked`)}>
+	Click me
+</div>
+
