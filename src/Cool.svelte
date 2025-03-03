00b1310d610e00b7f72748019802c2d40bfe9622
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2025-03-03 16:55:16 -0600

    clarify

--- src/Cool.svelte
+++ src/Cool.svelte
@@ -1,11 +1,14 @@
-<script lang='ts' generics='S extends string'>
-	export let whatever
+<script>
+	export let used
+
+	// THIS WARNING GETS LOGGED BY svelte-preprocess-esbuild, WHICH IS UNEXPECTED
 	// svelte-ignore export_let_unused
-	export let my_string: S
+	export let unused
 </script>
 
-{whatever}
+{used}
 
+<!-- This warning does not get logged, which is expected behavior -->
 <!-- svelte-ignore a11y_no_static_element_interactions a11y_click_events_have_key_events -->
 <div on:click={() => console.log(`clicked`)}>
 	Click me
